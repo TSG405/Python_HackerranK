d42c46d3ef69ba26e417065b0897a93cf88c269b
Tanmoy Sengupta <65300045+TSG405@users.noreply.github.com>
2020-10-28 12:15:06 +0530

    Create Counting Valleys.py

---
+++ Counting Valleys.py
@@ -0,0 +1,35 @@
+'''
+Problem Statement: https://www.hackerrank.com/challenges/counting-valleys/submissions/code/177309699
+@Coded by TSG,2020
+'''
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the countingValleys function below.
+def countingValleys(n, s):
+    level=valley=0
+    for i in range(n):
+        if(s[i]=='U'):
+            level+=1
+            if(level==0):
+                valley+=1
+        else:
+            level-=1
+    
+    return valley
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    s = input()
+
+    result = countingValleys(n, s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
