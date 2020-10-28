f1ae395a1ac799cba9f5fba5a3cacccc3e0f4662
Tanmoy Sengupta <65300045+TSG405@users.noreply.github.com>
2020-10-28 18:35:31 +0530

    Create Validating Postal Codes.py

---
+++ Validating Postal Codes.py
@@ -0,0 +1,7 @@
+'''
+Problem Statement: https://www.hackerrank.com/challenges/validating-postalcode/problem
+@Coded by TSG, 2020
+'''
+
+regex_integer_in_range = r'^[1-9][\d]{5}$'  # Do not delete 'r'.
+regex_alternating_repetitive_digit_pair = r'(\d)(?=\d\1)'  # Do not delete 'r'.
