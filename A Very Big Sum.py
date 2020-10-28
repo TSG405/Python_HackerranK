5709ab314a9c0ca35a89cfd7d95c72c4adde97dd
Tanmoy Sengupta <65300045+TSG405@users.noreply.github.com>
2020-10-28 12:07:24 +0530

    Update A Very Big Sum.py

--- A Very Big Sum.py
+++ A Very Big Sum.py
@@ -1,36 +1,6 @@
 '''
-In this challenge, you are required to calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
-Function Description
-
-Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
-
-aVeryBigSum has the following parameter(s):
-
-int ar[n]: an array of integers .
-Return
-
-long: the sum of all array elements
-Input Format
-
-The first line of the input consists of an integer n .
-The next line contains n space-separated integers contained in the array.
-
-Output Format
-
-Return the integer sum of the elements in the array.
-
-Constraints
-
-1<=n<=10
-0<=ar[i]<=10^10
-
-Sample Input
-
-5
-1000000001 1000000002 1000000003 1000000004 1000000005
-Output
-
-5000000015
+Problem Statement: https://www.hackerrank.com/challenges/a-very-big-sum/problem
+@Coded by TSG,2020
 '''
 
 import math
