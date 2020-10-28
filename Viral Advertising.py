f1dda3dacd758e4e4dffece400a3303f86a4780a
Tanmoy Sengupta <65300045+TSG405@users.noreply.github.com>
2020-10-28 12:20:59 +0530

    Create Viral Advertising.py

---
+++ Viral Advertising.py
@@ -0,0 +1,31 @@
+'''
+Problem Statement: https://www.hackerrank.com/challenges/strange-advertising/problem
+@Coded by TSG,2020
+'''
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the viralAdvertising function below.
+def viralAdvertising(n):
+    shared =5
+    cumulative=0
+    for i in range(1,n+1):
+        liked = shared//2
+        cumulative+=liked
+        shared = liked*3
+    return cumulative
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    result = viralAdvertising(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
