8778d77eba59ee7e657d675bdf9eab567464b6ce
Tanmoy Sengupta <65300045+TSG405@users.noreply.github.com>
2020-10-28 12:08:35 +0530

    Update Simple Array Sum.py

--- Simple Array Sum.py
+++ Simple Array Sum.py
@@ -1,33 +1,6 @@
 '''
-Given an array of integers, find the sum of its elements.
-For example, if the array ar=[1,2,3], 1+2+3=6 , so return 6.
-
-Function Description
-
-Complete the simpleArraySum function in the editor below. It must return the sum of the array elements as an integer.
-simpleArraySum has the following parameter(s):
-ar: an array of integers
-
-Input Format
-
-The first line contains an integer, , denoting the size of the array.
-The second line contains  space-separated integers representing the array's elements.
-
-Constraints
-
-0<n,ar[i]<=100
-
-Output Format
-
-Print the sum of the array's elements as a single integer.
-
-Sample Input
-
-6
-1 2 3 4 10 11
-Sample Output
-
-31
+Problem Statement: https://www.hackerrank.com/challenges/simple-array-sum/problem
+@Coded by TSG,2020
 '''
 
 
